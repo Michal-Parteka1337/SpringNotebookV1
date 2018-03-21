d7ae8b2a7232b08dc26e6a130e6c7e89167ab61a
Michal-Parteka1337 <michal.parteka94@gmail.com>
2018-03-21 14:52:30 +0100

    Clean up, first UI version
    Clean up, first UI version

--- src/main/java/notebook/repositiories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package notebook.repositiories;
-
-
-import notebook.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
--- src/main/java/notebook/services/UserService.java
+++
@@ -1,4 +0,0 @@
-package notebook.services;
-
-public class UserService {
-}
